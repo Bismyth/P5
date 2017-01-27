0ae6a0c5a365781ad2ddfdf3aaaa8414345ad8b3
Ben Woods <woodsy867@gmail.com>
2017-01-27 21:59:14 +0800

    Starting the actual level
    Starting to generate the actual pacman stage

--- Pacman/generate.js
+++ Pacman/generate.js
@@ -1,14 +1,55 @@
 function generate() {
   for (var i = 0; i < cols; i++) {
     cells[i][0].wall = true;
+    cells[i][height / w - 1].wall = true;
   }
-  //cells[4][1].wall = true;
+  for (var i = 0; i < 12; i++) {
+    cells[0][rows - (i + 1)].wall = true;
+    cells[cols - 1][rows - (i + 1)].wall = true;
+  }
+  for (var i = 0; i < 9; i++) {
+    cells[0][i + 1].wall = true;
+    cells[cols - 1][i + 1].wall = true;
+  }
+  for (var i = 0; i < 6; i++) {
+    cells[i][9 + 4].wall = true;
+    cells[i][9 + 6].wall = true;
+    cells[cols - (i + 1)][9 + 4].wall = true;
+    cells[cols - (i + 1)][9 + 6].wall = true;
+  }
+  for (var i = 0; i < 4; i++) {
+    cells[2 + i][2].wall = true;
+    cells[2 + i][3].wall = true;
+    cells[2 + i][4].wall = true;
+    cells[2 + i][6].wall = true;
+    cells[2 + i][7].wall = true;
+    cells[cols - (3 + i)][2].wall = true;
+    cells[cols - (3 + i)][3].wall = true;
+    cells[cols - (3 + i)][4].wall = true;
+    cells[cols - (3 + i)][6].wall = true;
+    cells[cols - (3 + i)][7].wall = true;
+
+    cells[cols / 2][i + 1].wall = true;
+    cells[cols / 2 - 1][i + 1].wall = true;
+  }
+  for (var i = 0; i < 5; i++) {
+    cells[7][14 + i].wall = true;
+    cells[8][14 + i].wall = true;
+    cells[cols - 8][14 + i].wall = true;
+    cells[cols - 9][14 + i].wall = true;
+    cells[i + 1][9].wall = true;
+    cells[i + 7][2].wall = true;
+    cells[i + 7][3].wall = true;
+    cells[i + 7][4].wall = true;
+    cells[cols - (i + 8)][2].wall = true;
+    cells[cols - (i + 8)][3].wall = true;
+    cells[cols - (i + 8)][4].wall = true;
+    //cells[i + 1][rows].wall = true;
+  }
+  for (var i = 0; i < 8; i++) {}
   for (var i = 0; i < cols; i++) {
-    cells[i][2].wall = true;
+    for (var j = 0; j < rows; j++) {
+      cells[i][j].check();
+    }
   }
-  cells[3][2].wall = false;
-  for (var i = 0; i < cols; i++) {
-    cells[i][4].wall = true;
-  }
-  cells[5][4].wall = false;
 }
