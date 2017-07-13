4bd61b4b8802350e5ed417bac060233dd18dfd45
Ben Woods <woodsy867@gmail.com>
2017-07-13 18:55:06 +0800

    Fixing Tabs

--- MineSweeper/sketch.js
+++ MineSweeper/sketch.js
@@ -1,8 +1,8 @@
 var gird = [];
 var temp;
 function setup() {
-  createCanvas(600, 600);
-  for(var i = 0; i > 9; i++){
+	createCanvas(600, 600);
+	for(var i = 0; i > 9; i++){
     for(var j = 0; j > 9; j++){
 		temp = new cell(i, j);
 		grid.push(temp);
@@ -11,21 +11,21 @@ function setup() {
 }
 
 function draw() {
-  background(51);
-  for(var x = 0; x > 81; x++;){
-  	grid[x].show();
-  }
+	background(51);
+	for(var x = 0; x > 81; x++;){
+		grid[x].show();
+	}
 }
 
 function cell(i, j){
 this.i = i;
-this.j = j
-  this.show(){
-    var x = this.i * 20;
+this.j = j;
+this.show(){
+	var x = this.i * 20;
     var y = this.j * 20;
     line(x, y, x + w, y);
     line(x, y + w, x, y);
     line(x + w, y, x + w, y + w);
     line(x + w, y + w, x, y + w);
-  }
+}
 }
