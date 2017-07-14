6aa1bda709c3292c9f210e6baff2019bf9471684
Ben Woods <woodsy867@gmail.com>
2017-07-14 21:50:01 +0800

    Adding Check Functionality
    Not Working yet just want to save to github

--- MineSweeper/sketch.js
+++ MineSweeper/sketch.js
@@ -4,6 +4,8 @@ var temp, w;
 var cellT = [];
 var mouseCellX, mouseCellY;
 var go = false;
+var testX = 0, testY = 0;
+
 function setup() {
 	createCanvas(603, 603);
 	for(var x = 0; x < grid.length; x++){
@@ -16,6 +18,7 @@ function setup() {
 			grid[i][j] = (temp);
 		}
 	}
+	
 	// for(var i = 0; i < 3; i++){
 		// for(var j = 0; j < 3; j++){
 			// for(var h = 0; h < 3; h++){
@@ -38,20 +41,26 @@ function draw() {
 			
 		}
 	}
-	grid[0][2].value = 3;
-	grid[2][4].value = 5;
+	
 }
 
 function mouseClicked() {
-  mouseCellX = floor(mouseX/(width/9));
-  mouseCellY = floor(mouseY/(height/9));
-  grid[mouseCellX][mouseCellY].background = 75;
-  go = true;
-  return false;
+	if (go) {
+		grid[mouseCellX][mouseCellY].background = 53;
+	}
+	if(mouseX < width && mouseY < height){
+		mouseCellX = floor(mouseX/(width/9));
+		mouseCellY = floor(mouseY/(height/9));
+		if(grid[mouseCellX][mouseCellY].pValue == undefined){
+			grid[mouseCellX][mouseCellY].background = 75;
+			go = true; 
+		}
+	}
+	return false;
 }
 
 function keyPressed() {
-	if(go) {
+	if(go && keyCode > 48 && keyCode < 58) {
 		grid[mouseCellX][mouseCellY].value = keyCode - 48;
 		grid[mouseCellX][mouseCellY].background = 53;
 		go = false;
@@ -62,6 +71,7 @@ function cell(i, j){
 this.i = i;
 this.j = j;
 this.value;
+this.pValue;
 this.background = [51,51,51];
 this.show = function(){
 	stroke(255);
@@ -69,9 +79,67 @@ this.show = function(){
 	var x = this.i * w;
  	var y = this.j * w;
     rect(x,y,w,w);
-	if(this.value != undefined){
-		fill(255);
-		text(this.value, x + 17, y,x+w,y+w);
-	}
+	fill(255);
+	if(this.i == 2){ rect(x+w-3,y,w,w); }
+	if(this.i == 5){ rect(x+w-3,y,w,w); }
+	if(this.j == 2){ rect(x,y+w-3,w,w); }
+	if(this.j == 5){ rect(x,y+w-3,w,w); }
+	if(this.value != undefined){ text(this.value, x + 17, y,x+w,y+w);}
+	if(this.pValue != undefined){ text(this.pValue, x + 17, y,x+w,y+w);}
 }
+}
+
+function check(){
+	bCell = [];
+	for(var i = 0; i < 3; i++){
+		for(var j = 0; j < 3; j++){
+			for(var h = 0; h < 3; h++){
+				for(var k = 0; k < 3; k++){
+					cellT.push(grid[i*3+k][j*3+h]);
+				}
+			}
+			bCell.push(cellT);
+			cellT = [];
+		}
+	}
+	for(var i = 0; i < grid.length; i++){
+		for(var j = 0; j < grid[i].length; j++){
+			if(grid[i][j].value == undefined){
+				return false;
+			} else {
+				testX += grid[i][j].value;
+				testY += grid[j][i].value;
+			}
+		}
+		if(testX == 45 && testY == 45){
+			testX = 0;
+			testY = 0;
+		} else {
+			return false;
+		}
+	}
+	testX = 0;
+	for(var i = 0; i < bCell.length; i++){
+		for(var j = 0; j < bCell[i].length; j++){
+			testX += bCell[i][j].value;
+		}
+		if(testX == 45){
+			testX = 0;
+		} else {
+			return false;
+		}
+	}
+	return true;
+}
+
+function ftc(){
+	for(var i =0; i < grid.length; i++){
+		for(var j =0; j < grid[i].length; j++){
+			if(j + 1 + i - 9 > 0){
+				grid[i][j].value = j + 1 + i - 9;
+			} else {
+				grid[i][j].value = j + 1 + i;
+			}
+		}
+	}
 }
