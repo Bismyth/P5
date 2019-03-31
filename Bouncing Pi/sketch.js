a1a830a70a689fe2405e02d2931bacaa4beb07b2
Ben Woods <woodsy867@gmail.com>
2019-03-31 17:12:53 +0800

    Adding Buttons

--- Bouncing Pi/sketch.js
+++ Bouncing Pi/sketch.js
@@ -2,34 +2,36 @@ fr = 50;
 wl = 10;
 coll = 0;
 n = 9;
-iter = 10000000;
+iter = 1000000;
 finishup = 100;
 ended = false;
 run = false;
 function setup() {
   createCanvas(800, 400);
   block1 = new Block(50+wl,10,0,1);
+  block2 = new Block(200+wl,50,-1/iter,0);
   countDiv = createDiv();
   digit = createSlider(1,9,n,1);
   digitDiv = createDiv();
-  button = createButton('submit');
-  button.mousePressed(start);
+  sb = createButton('Start');
+  sb.mousePressed(start);
+  rb = createButton('Reset');
+  rb.mousePressed(reset)  ;
 }
 
 function draw() {
   background(51);
   scene(fr,wl);
-  if(run){
-    block2 = new Block(200+wl,50,-1/iter,100**(n-1));
+  n = digit.value();
+  if(run){  
     if(!ended || finishup > 0 ){
       for (x = 0; x < iter;x++){
         block1.update();
         block2.update();
         block1.collide(block2);
       }
-      ended = block1.v > 0 && block2.v >= block1.v;
+      ended = block1.v >= 0 && block2.v >= block1.v;
       finishup = ended ? finishup-1 : finishup;
-
     }else {
       stroke(255);
       fill(255);
@@ -38,7 +40,7 @@ function draw() {
   }
   block1.show();
   block2.show();
-  countDiv.html("Collisions:&nbsp"+ coll + "<br>Actual:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp"+floor(PI*10**(n-1)));
+  countDiv.html("Collisions:&nbsp"+ coll + "<br>Actual:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp"+floor(PI*10**(n-1)));
   digitDiv.html("Digits of Pi: " + n);
 }
 
