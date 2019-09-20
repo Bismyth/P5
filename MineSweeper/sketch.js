394af5ea0e11cc12cc8c333a8036197020756e2e
Ben Woods <woodsy867@gmail.com>
2019-09-21 01:19:26 +0800

    Reorganising Filesystem

--- MineSweeper/sketch.js
+++ MineSweeper/sketch.js
@@ -1,176 +1,96 @@
-var grid = new Array(9);
-var bCell = [];
-var temp, w;
-var cellT = [];
-var mouseCellX, mouseCellY;
-var go = false;
-var testX = 0, testY = 0;
-
+var cols = 10;
+var cellSize = 50;
+var grid = [];
+var empties = [];
+b = 69;
 function setup() {
-	createCanvas(603, 603);
-	for(var x = 0; x < grid.length; x++){
-		grid[x] = new Array(9);
-	}
-	w = floor(603/9);
-	for(var i = 0; i < 9; i++){
-		for(var j = 0; j < 9; j++){
-			temp = new cell(i, j);
-			grid[i][j] = (temp);
-		}
-	}
-	ftc();
-	// for(var i = 0; i < 3; i++){
-		// for(var j = 0; j < 3; j++){
-			// for(var h = 0; h < 3; h++){
-				// for(var k = 0; k < 3; k++){
-					// cellT.push(grid[i*3+k][j*3+h]);
-				// }
-			// }
-			// bCell.push(cellT);
-			// cellT = [];
-		// }
-	// }
+  createCanvas(cols*cellSize, cols*cellSize);
+  for (var x=0;x<cols;x++){
+    grid.push([])
+    for (var y=0;y<cols;y++){
+      grid[x].push(new Cell(x,y));
+    }
+  }
+  generateMinefield(cols,10).forEach((x) => {grid[x[0]][x[1]].type = 'mine';});
+  grid.forEach((x) =>{x.forEach((y)=>{
+    c = findingProx(grid,y.x,y.y);
+    if (c > 0){
+      y.type = 'close';
+      y.prox = c;
+    }
+  });});
+  grid.forEach((x) =>{x.forEach((y)=>{
+    findingEmp(grid,y.x,y.y);
+  });});
 }
 
 function draw() {
-	background(51);
-	textSize(60);
-	for(var i = 0; i < grid.length; i++){
-		for(var j = 0; j < grid[i].length; j++){
-			grid[i][j].show();
-			
-		}
-	}
-	
+  background(51);
+  grid.forEach((x) =>{x.forEach((y)=>{
+    y.show();
+  });});
 }
 
-function mouseClicked() {
-	if (go) {
-		grid[mouseCellX][mouseCellY].background = 53;
-	}
-	if(mouseX < width && mouseY < height){
-		mouseCellX = floor(mouseX/(width/9));
-		mouseCellY = floor(mouseY/(height/9));
-		if(grid[mouseCellX][mouseCellY].pValue == undefined){
-			grid[mouseCellX][mouseCellY].background = 75;
-			go = true; 
-		}
-	}
-	return false;
+function generateMinefield(c,n){
+  if (c*c <= n){
+    console.error("More mines than field size");
+    return -1;
+  }
+  pos = [];
+  mines = [];
+  for (var x=0;x<c;x++){
+    for (var y=0;y<c;y++){
+      pos.push([x,y]);
+    }
+  }
+  for(var x = 0;x<n;x++){
+    mines.push(pos.splice(Math.floor(Math.random() * pos.length),1)[0]);
+  }
+  mines.sort();
+  return mines;
+}
+function findingProx(myArray, i, j) {
+  if (myArray[i][j].type == 'mine'){
+    return 0;
+  }
+  var rowLimit = myArray.length-1;
+  var columnLimit = myArray[0].length-1;
+  n = 0;
+  for(var x = Math.max(0, i-1); x <= Math.min(i+1, rowLimit); x++) {
+    for(var y = Math.max(0, j-1); y <= Math.min(j+1, columnLimit); y++) {
+      if(x !== i || y !== j) {
+        if(myArray[x][y].type == 'mine'){
+          n++;
+        }
+      }
+    }
+  }
+  return n;
 }
 
-function keyPressed() {
-	if(go && keyCode > 48 && keyCode < 58) {
-		grid[mouseCellX][mouseCellY].value = keyCode - 48;
-		grid[mouseCellX][mouseCellY].background = 53;
-		go = false;
-	}
+
+function Cell(x,y){
+  this.x = x;
+  this.y = y;
+  this.type = null;
+  this.show = function(){
+    noStroke();
+    if(this.type == 'mine'){
+      fill(255,0,0);
+    } else {
+      fill(0,128,0);
+    }
+    rect(this.x*cellSize,this.y*cellSize,cellSize,cellSize);
+    if (this.type == 'close'){
+      fill(255);
+      textAlign(CENTER, CENTER);
+      text(this.prox.toString(),this.x*cellSize,this.y*cellSize,cellSize,cellSize);
+    }
+    if (this.type == 'empty'){
+      fill(255);
+      textAlign(CENTER, CENTER);
+      text(this.ec.toString(),this.x*cellSize,this.y*cellSize,cellSize,cellSize);
+    }
+  }
 }
 
-function cell(i, j){
-this.i = i;
-this.j = j;
-this.value;
-this.pValue;
-this.background = [51,51,51];
-this.show = function(){
-	stroke(255);
-	fill(this.background);
-	var x = this.i * w;
- 	var y = this.j * w;
-    rect(x,y,w,w);
-	fill(255);
-	if(this.i == 2){ rect(x+w-3,y,w,w); }
-	if(this.i == 5){ rect(x+w-3,y,w,w); }
-	if(this.j == 2){ rect(x,y+w-3,w,w); }
-	if(this.j == 5){ rect(x,y+w-3,w,w); }
-	if(this.value != undefined){ text(this.value, x + 17, y,x+w,y+w);}
-	if(this.pValue != undefined){ text(this.pValue, x + 17, y,x+w,y+w);}
-}
-}
-
-function check(){
-	bCell = [];
-	testX = 0;
-	testY = 0;
-	for(var i = 0; i < 3; i++){
-		for(var j = 0; j < 3; j++){
-			for(var h = 0; h < 3; h++){
-				for(var k = 0; k < 3; k++){
-					cellT.push(grid[i*3+k][j*3+h]);
-				}
-			}
-			bCell.push(cellT);
-			cellT = [];
-		}
-	}
-	for(var i = 0; i < grid.length; i++){
-		for(var j = 0; j < grid[i].length; j++){
-			if(grid[i][j].value == undefined){
-				return false;
-			} else {
-				testX += grid[i][j].value;
-				testY += grid[j][i].value;
-			}
-		}
-		if(testX == 45 && testY == 45){
-			testX = 0;
-			testY = 0;
-		} else {
-			return false;
-		}
-	}
-	testX = 0;
-	for(var i = 0; i < bCell.length; i++){
-		for(var j = 0; j < bCell[i].length; j++){
-			testX += bCell[i][j].value;
-		}
-		if(testX == 45){
-			testX = 0;
-		} else {
-			return false;
-		}
-	}
-	return true;
-}
-
-function ftc(){
-	for(var i =0; i < grid.length; i++){
-		for(var j =0; j < grid[i].length; j++){
-			if(j > 5){
-				if(j*3 + 3 + i > 9){
-					if(j * 3 + 3 + i - 9 > 9){
-						if (j * 3 + 3 + i - 18 > 9) {
-							grid[i][j].value = j * 3 + 3 + i - 27;
-						} else {
-							grid[i][j].value = j * 3 + 3 + i - 18;
-						}
-					} else {
-						grid[i][j].value = j * 3 + 3 + i - 9;
-					}
-				} else {
-					grid[i][j].value = j * 3 + 3 + i;
-				}
-			} else if (j > 2) {
-				// if(j*3 + 3 + i > 18){
-					// grid[i][j].value = j * 3 + 3 + i - 18;
-				// } else 
-				if(j*3 + 2 + i > 9){
-					if(j * 3 + 2 + i - 9 > 9){
-						grid[i][j].value = j * 3 + 2 + i - 18;
-					} else {
-						grid[i][j].value = j * 3 + 2 + i - 9;
-					}
-				} else {
-					grid[i][j].value = j * 3 + 2 + i;
-				}
-			} else {
-				if(j * 3 + 1 + i > 9){
-					grid[i][j].value = j * 3 + 1 + i - 9;
-				} else {
-					grid[i][j].value = j * 3+ 1 + i;
-				}
-			}
-		}
-	}
-}
